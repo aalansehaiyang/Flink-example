83a8fd53681db980a4d6006508234a6338acf2d1
onlyone <onlyone@192.168.0.103>
2020-08-21 11:18:29 +0800

    flink 安装、demo程序

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Flink-example
+---
+
+* [Flink 安装](doc/1.md)
---
+++ flink-basic/src/main/java/com/onlyone/streaming/SocketWindowWordCount.java
@@ -0,0 +1,88 @@
+package com.onlyone.streaming;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.functions.ReduceFunction;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.util.Collector;
+
+/**
+ * @Author onlyone
+ * @create 2020/8/21
+ */
+public class SocketWindowWordCount {
+    public static void main(String[] args) throws Exception {
+
+        // the host and the port to connect to
+        final String hostname;
+        final int port;
+        try {
+            final ParameterTool params = ParameterTool.fromArgs(args);
+            hostname = params.has("hostname") ? params.get("hostname") : "localhost";
+//            port = params.getInt("port");
+            port = 9200;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+
+        // get the execution environment
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        // 从socket套接字中获取数据流
+        DataStream<String> text = env.socketTextStream(hostname, port, "\n");
+
+        // parse the data, group it, window it, and aggregate the counts
+        DataStream<WordWithCount> windowCounts = text
+
+                .flatMap(new FlatMapFunction<String, WordWithCount>() {
+                    @Override
+                    public void flatMap(String value, Collector<WordWithCount> out) {
+                        for (String word : value.split("\\s")) {
+                            out.collect(new WordWithCount(word, 1L));
+                        }
+                    }
+                })
+
+                .keyBy("word")  // 按照 word 字段聚合
+                .timeWindow(Time.seconds(60), Time.seconds(3))  // 数据时间窗口大小为2s，输出时间间隔为3s
+
+                .reduce(new ReduceFunction<WordWithCount>() {
+                    @Override
+                    public WordWithCount reduce(WordWithCount a, WordWithCount b) {
+                        return new WordWithCount(a.word, a.count + b.count);
+                    }
+                });
+
+        // print the results with a single thread, rather than in parallel
+        windowCounts.print().setParallelism(1);
+
+        env.execute("Socket Window WordCount");
+    }
+
+    // ------------------------------------------------------------------------
+
+    /**
+     * Data type for words with count.
+     */
+    public static class WordWithCount {
+
+        public String word;
+        public long count;
+
+        public WordWithCount() {
+        }
+
+        public WordWithCount(String word, long count) {
+            this.word = word;
+            this.count = count;
+        }
+
+        @Override
+        public String toString() {
+            return word + " : " + count;
+        }
+    }
+}
